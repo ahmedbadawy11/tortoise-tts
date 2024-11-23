bc89b937ae9da414077e764efb942901bd6465e6
Ahmed Ibraheem Ahmed Ali <ahmedibraheem300@gmail.com>
2024-11-23 17:22:39 +0200

    allow speech generation

--- tortoise/utils/tokenizer.py
+++ tortoise/utils/tokenizer.py
@@ -4,7 +4,7 @@ import re
 import inflect
 import torch
 from tokenizers import Tokenizer
-
+from arabic2latin import arabic_to_latin 
 
 # Regular expression matching whitespace:
 from unidecode import unidecode
@@ -141,10 +141,11 @@ def transliteration_cleaners(text):
 
 def english_cleaners(text):
   '''Pipeline for English text, including number and abbreviation expansion.'''
-  text = convert_to_ascii(text)
+  # text = convert_to_ascii(text)
+  text = arabic_to_latin(text)
   text = lowercase(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
+  # text = expand_numbers(text)
+  # text = expand_abbreviations(text)
   text = collapse_whitespace(text)
   text = text.replace('"', '')
   return text
